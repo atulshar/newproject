92a43fac7f091b33e94315e4883995db428111a3
atulshar <atul.b.sharma@capgemini.com>
2018-04-11 16:29:31 +0530

    Changed

--- FirstPython.py
+++ FirstPython.py
@@ -7,9 +7,9 @@ outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
 inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
                                     # the inbox. You can change that number to reference
-       AAAAAAAAAA                             # any other folder
+                             # any other folder
 messages = inbox.Items
 message = messages.GetLast()
 body_content = message.body
 
-NEW FUNCITON SJHSSHSJH
+
